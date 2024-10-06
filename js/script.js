0ca4c02a04235869beafa7748a0d389e5f559ae8
Ayush Jaiswal <mrayushj14092002@gmail.com>
2024-10-06 14:48:14 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//Logic Starts building
+
 let navbarDiv = document.querySelector('.navbar');
 window.addEventListener('scroll', () => {
     if(document.body.scrollTop > 40 || document.documentElement.scrollTop > 40){
